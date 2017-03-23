c57dc6988407c59519a76119ae9fd8cdec0cd2b3
Connorelsea <connorelsea@gmail.com>
2017-03-23 12:43:04 -0500

    Updated html page structure

--- scraping-server/index.js
+++ scraping-server/index.js
@@ -1,13 +1,9 @@
 const osmosis = require("osmosis");
 
 function scrapeCourses() {
-  osmosis
-    .get("http://appl101.lsu.edu/booklet2.nsf/mainframeset")
-    .data(function(d) {
-      console.log(d);
-    })
-    .log(console.log)
-    .debug(console.log);
+  const website = "http://htmlpreview.github.io/?https://github.com/Connorelsea/node-scraping-talk/blob/master/college-website/index.html";
+
+  osmosis.get(website);
 }
 
 scrapeCourses();
