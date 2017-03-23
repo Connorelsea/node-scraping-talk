6f82a9dfe3dff8a174f73acbf60acbc33b1db9f1
Connorelsea <connorelsea@gmail.com>
2017-03-23 13:01:53 -0500

    Added doctype

--- scraping-server/index.js
+++ scraping-server/index.js
@@ -1,9 +1,30 @@
+const cheerio = require("cheerio");
+const fetch = require("isomorphic-fetch");
 const osmosis = require("osmosis");
 
 function scrapeCourses() {
-  const website = "http://htmlpreview.github.io/?https://github.com/Connorelsea/node-scraping-talk/blob/master/college-website/index.html";
+  const website = "https://connorelsea.github.io/node-scraping-talk/college-website/";
 
-  osmosis.get(website);
+  fetch(website).then(result => {
+    let $ = cheerio.load(result);
+
+    console.log($("body").html());
+  });
+
+  // osmosis
+  //   .get(website)
+  //   .find("body")
+  //   .set({ body: ":source" })
+  //   .set({
+  //     department: ".course__dept",
+  //     name: ".course_name",
+  //     number: ".course_number",
+  //     hours: ".course__hours",
+  //     instructor: ".course__instructor"
+  //   })
+  //   .data(d => console.log(d))
+  //   .debug(console.log)
+  //   .error(console.log);
 }
 
 scrapeCourses();
