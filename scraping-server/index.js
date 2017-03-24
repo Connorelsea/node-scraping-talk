b871f68e0a789bd6fc0b2fea4de21b36a036d596
Connorelsea <connorelsea@gmail.com>
2017-03-24 08:34:46 -0500

    Added variable number of instructors

--- scraping-server/index.js
+++ scraping-server/index.js
@@ -1,30 +1,34 @@
 const cheerio = require("cheerio");
-const fetch = require("isomorphic-fetch");
+const request = require("request-promise");
 const osmosis = require("osmosis");
 
 function scrapeCourses() {
-  const website = "https://connorelsea.github.io/node-scraping-talk/college-website/";
+  let website = "https://connorelsea.github.io/node-scraping-talk/college-website/";
 
-  fetch(website).then(result => {
-    let $ = cheerio.load(result);
+  request(website)
+    .then(result => {
+      let $ = cheerio.load(result);
 
-    console.log($("body").html());
-  });
+      let rows = $(".courses-table").find("tr");
+      let courses = [];
 
-  // osmosis
-  //   .get(website)
-  //   .find("body")
-  //   .set({ body: ":source" })
-  //   .set({
-  //     department: ".course__dept",
-  //     name: ".course_name",
-  //     number: ".course_number",
-  //     hours: ".course__hours",
-  //     instructor: ".course__instructor"
-  //   })
-  //   .data(d => console.log(d))
-  //   .debug(console.log)
-  //   .error(console.log);
+      $(rows).each(function(i, element) {
+        courses.push({
+          dept: $(element).find(".course__dept").text(),
+          name: $(element).find(".course__name").text(),
+          num: $(element).find(".course__num").text(),
+          hours: $(element).find(".course__hours").text(),
+          inst: $(element).find(".course__inst").text()
+        });
+      });
+
+      console.log(courses.filter(course => course.num.startsWith("3")));
+
+      console.log(courses.filter(course => course.dept === "CSC"));
+
+      //console.log(courses);
+    })
+    .catch(error => console.log(error));
 }
 
 scrapeCourses();
